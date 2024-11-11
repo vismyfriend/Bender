42fd80496bf86be31d35003d873dca2d6bbf82e2
Vismyfriend <vismyfriend@yahoo.com>
2024-11-11 07:02:37 +0300

    link to zoom lessons1

--- scripts/utils/linksToHW.js
+++ scripts/utils/linksToHW.js
@@ -1,5 +1,6 @@
 const linksToHW = {
     devmode0: "https://us06web.zoom.us/j/9041113793?pwd=cmZpZlpQZXRhYkh4RW9JTzZoTTZXZz09",
+    diy: "https://us06web.zoom.us/j/9041113793?pwd=cmZpZlpQZXRhYkh4RW9JTzZoTTZXZz09",
     suchSo: "https://www.yahoo.com",
 }
 
